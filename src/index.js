c00e48741231bb7cecc69b68156777beb5b8a718
Viktor <kosenkovv89@gmail.com>
2020-09-10 11:14:59 +0300

    7.105 - done

--- src/index.js
+++ src/index.js
@@ -4,24 +4,40 @@ const reducer = (state = 0, action) => {
   switch (action.type) {
     case 'INC':
       return ++state;
+    case 'DEC':
+      return --state;
+    case 'RND':
+      return state + action.value;
     default:
       return state;
   }
 }
 
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const rnd = (value) => ({type: 'RND', value});
+
 const store = createStore(reducer);
-store.subscribe(() => {
-  console.log(store.getState());
-})
 
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
-store.dispatch({type: 'INC'});
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+});
 
-// let state = reducer(undefined, {});
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+});
 
-// state = reducer(state, {type: 'INC'});
-// console.log(state);
-// state = reducer(state, {type: 'INC'});
-// console.log(state);
+document.getElementById('rnd').addEventListener('click', () => {
+  const value = Math.floor(Math.random() * 10);
+  store.dispatch(rnd(value));
+});
 
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
+
+store.subscribe(update);
+
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
+// store.dispatch({type: 'INC'});
