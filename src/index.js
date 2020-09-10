1bb344f80694388e6e374e1b4c0e4f32164160e6
Viktor <kosenkovv89@gmail.com>
2020-09-10 14:18:44 +0300

    7.106 - done

--- src/index.js
+++ src/index.js
@@ -6,38 +6,34 @@ const reducer = (state = 0, action) => {
       return ++state;
     case 'DEC':
       return --state;
-    case 'RND':
-      return state + action.value;
+    case 'RESET':
+      return 0;
     default:
       return state;
   }
 }
 
-// const inc = () => ({type: 'INC'});
-// const dec = () => ({type: 'DEC'});
-// const rnd = (value) => ({type: 'RND', value});
+const inc = () => ({type: 'INC'});
+const dec = () => ({type: 'DEC'});
+const reset = () => ({type: 'RESET'});
 
-// const store = createStore(reducer);
+const store = createStore(reducer);
 
-// document.getElementById('inc').addEventListener('click', () => {
-//   store.dispatch(inc());
-// });
+document.getElementById('inc').addEventListener('click', () => {
+  store.dispatch(inc());
+});
 
-// document.getElementById('dec').addEventListener('click', () => {
-//   store.dispatch(dec());
-// });
+document.getElementById('dec').addEventListener('click', () => {
+  store.dispatch(dec());
+});
 
-// document.getElementById('rnd').addEventListener('click', () => {
-//   const value = Math.floor(Math.random() * 10);
-//   store.dispatch(rnd(value));
-// });
+document.getElementById('reset').addEventListener('click', () => {
+  store.dispatch(reset());
+});
 
-// const update = () => {
-//   document.getElementById('counter').textContent = store.getState();
-// }
+const update = () => {
+  document.getElementById('counter').textContent = store.getState();
+}
 
-// store.subscribe(update);
+store.subscribe(update);
 
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'INC'});
-// store.dispatch({type: 'INC'});
